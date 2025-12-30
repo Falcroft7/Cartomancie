2e183ee38cb1e4a12c609c913a2f29b35484c319
Falcroft7 <kevin.patry22@gmail.com>
2025-12-30 14:17:28 +0100

    Update tarot_script.js

--- tarot_script.js
+++ tarot_script.js
@@ -303,7 +303,6 @@ function affichTirageDetail(tirage, categorie) {
   tirage.positions.forEach(pos => {
     const carte = document.createElement("div");
     carte.className = "tirage-carte";
-    carte.style.position = "absolute";
     carte.innerHTML = `
       <img src="Images/Dos_carte.png" class="tirage-carte-image">
       <p>${pos.label}</p>
@@ -311,15 +310,11 @@ function affichTirageDetail(tirage, categorie) {
 
     if ("x" in pos && "y" in pos) {
       // Tirage à grille
-      let x = pos.x + 1;
-      let y = pos.y + 1;
-      if (x < 1) x = 1;
-      if (y < 1) y = 1;
-      carte.style.setProperty("--x", x);
-      carte.style.setProperty("--y", y);
-      carte.style.position = "";
+      carte.style.gridColumn = pos.x + 1;
+      carte.style.gridRow = pos.y + 1;
     } else if ("angle" in pos && "radius" in pos) {
       // Tirage circulaire
+      carte.style.position = "absolute";
       const radiusPx = pos.radius * 60;
       const angleRad = (pos.angle * Math.PI) / 180;
       const centerX = 150;
@@ -383,3 +378,4 @@ Papa.parse(csvTiragesUrl, {
     });
   }
 });
+

76fc98f6fba5c7afebb770221725ce7f591fc606
Falcroft7 <kevin.patry22@gmail.com>
2026-01-20 14:30:04 +0100

    Add constants and initial data structures

--- js/constants.js
+++ js/constants.js
@@ -1,1 +1,10 @@
+/* =========== CONSTANTES =========== */
+const csvArcanesUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjeBMuYH_nr0z9h1GL9FnU_2XzEeNMZQ4dVsCMeQVZfw5tXacWxY9VC_GdbONB2ZCzo4EVsnrGHwtC/pub?output=csv";
+const csvTiragesUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjeBMuYH_nr0z9h1GL9FnU_2XzEeNMZQ4dVsCMeQVZfw5tXacWxY9VC_GdbONB2ZCzo4EVsnrGHwtC/pub?gid=1625499577&single=true&output=csv";
 
+/* =========== DONNÉES =========== */
+let listeMajors = [];
+let listeMinors = [];
+let tiragesCategorie = [];

1a2b9774bf6dda2e6d9ef71d52d0192a0dcf0406
Falcroft7 <kevin.patry22@gmail.com>
2026-08-31 17:04:27 +0200

    Add csvDecksUrl for additional data source

--- js/config.js
+++ js/config.js
@@ -3,6 +3,8 @@ const csvArcanesUrl =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjeBMuYH_nr0z9h1GL9FnU_2XzEeNMZQ4dVsCMeQVZfw5tXacWxY9VC_GdbONB2ZCzo4EVsnrGHwtC/pub?output=csv";
 const csvTiragesUrl =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjeBMuYH_nr0z9h1GL9FnU_2XzEeNMZQ4dVsCMeQVZfw5tXacWxY9VC_GdbONB2ZCzo4EVsnrGHwtC/pub?gid=1625499577&single=true&output=csv";
+const csvDecksUrl =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTjeBMuYH_nr0z9h1GL9FnU_2XzEeNMZQ4dVsCMeQVZfw5tXacWxY9VC_GdbONB2ZCzo4EVsnrGHwtC/pub?gid=1818382291&single=true&output=csv";
 
 /* =========== VARIABLES DE DONNÉES GLOBALES =========== */
 let listeMajors = [];
